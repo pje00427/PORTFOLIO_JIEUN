a3a55386ca6dde8f905d4fa279de9ba4b457fbb1
pje00427 <pje00427@naver.com>
2023-10-06 15:16:09 +0900

    스크립트 수정

--- src/type.js
+++ src/type.js
@@ -4,11 +4,11 @@ new TypeIt('.home__title--strong', {
   loop: true,
   speed: 100,
 }) // Dream Coder|
-  .move(-11)
-  // .type('Amazing ') //Amazing |Dream Coder
-  // .pause(1000)
-  .move(null, { to: 'END' }) //Amazing Dream Coder|
-  .delete() //|
+  // .move(-11)
+  // // .type('Amazing ') //Amazing |Dream Coder
+  // // .pause(1000)
+  // .move(null, { to: 'END' }) //Amazing Dream Coder|
+  // .delete() //|
   .type('Front-end Engineer') //Front-end Engineer|
   .pause(1000)
   .move(-9) //Front-end| Engineer
