8095b24a9ba00ab589f7e263b82f3b681eadf28b
pje00427 <pje00427@naver.com>
2023-10-16 17:00:54 +0900

    js 수정

--- src/type.js
+++ src/type.js
@@ -9,11 +9,11 @@ new TypeIt('.home__title--strong', {
   // // .pause(1000)
   .move(null, { to: 'END' }) //Amazing Dream Coder|
   .delete() 
-  .type('Front-end Engineer') //Front-end Engineer|
+  .type('Back-end Engineer') //Front-end Engineer|
   .pause(1000)
   .move(-9) //Front-end| Engineer
   .delete(9) //| Engineer
-  .type('Back-end') //Back-end| Engineer
+  .type('Front-end') //Back-end| Engineer
   .pause(1000)
   .delete(8) //| Engineer
   .type('Full-stack') //Full-stack| Engineer
